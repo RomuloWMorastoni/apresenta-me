67e3b3cc103f473ad7cc0342c30577f98c19dcae
RomuloWMorastoni <romulo_rsl@hotmail.com>
2024-12-07 11:54:11 -0300

    Criado o controller PessoasController utilizando o artisan

--- app/Http/Controllers/PessoasController.php
+++ app/Http/Controllers/PessoasController.php
@@ -6,21 +6,59 @@ use Illuminate\Http\Request;
 
 class PessoasController extends Controller
 {
-    public function listarPessoas(){
-        $pessoas = [
-            'Rômulo',
-            'Teste',
-            'Silva',
-        ];
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
 
-        $html = "<ul>";
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
 
-        foreach($pessoas as $user){
-            $html .= "<li>$user</li>";
-        }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
-        $html .= "</ul>";
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
 
-        echo $html;
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
